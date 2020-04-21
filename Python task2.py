5c06929bd64768e3992275702892cec938668239
Codeflour <cynthianuchepro@gmail.com>
2020-04-21 10:59:13 +0100

    first

--- Python task2.py
+++ Python task2.py
@@ -3,4 +3,10 @@
 import string
 import random
 
-def get_details ()
+def user_details ()
+
+first_name=imput ('Enter your first name: ')
+last_name=imput ('Enter your last name: ')
+email=imput ('Enter your email: ')
+
+if 
