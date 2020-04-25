4dd3e0e4204f28e5eaf164587f219bbb01a0d601
Codeflour <cynthianuchepro@gmail.com>
2020-04-25 12:17:39 +0100

    second commit

--- Python task2.py
+++ Python task2.py
@@ -2,11 +2,48 @@
 
 import string
 import random
-
-def user_details ()
+container= []
+#def user_details ():
 
 first_name=input ('Enter your first name: ')
-last_name=input ('Enter your last name: ')
-email=input ('Enter your email: ')
+container.append("First Name: " + first_name)
 
-if 
+last_name=input ('Enter your last name: '  )
+container.append("Last Name: " + last_name)
+
+email=input ('Enter your email: ')
+container.append("Email: " + email)
+
+user_string = (first_name [0:2] + last_name[-2:])
+  
+
+#def random_alphaNumeric(string_length):
+  
+lettersAndDigits = string.ascii_letters + string.digits
+random_string = ''.join((random.choice(lettersAndDigits)
+for i in range(5)))
+
+
+userpassword = user_string + random_string
+
+print("""
+  
+Kindly respond with a 'yes' or 'no': 
+   
+   """)
+print("Is this password '" +userpassword + "' fine by you? ")
+response = input().lower()
+
+if response == "yes":
+  container.append("Password: " + userpassword)
+  print("These are your details:")
+  for item in container:
+    print(item)
+
+elif response == "no":
+  print("Okay then! Choose a password of your choice: ")
+  password= input()
+
+else:
+  print("Can you kindly enter a yes or no?")
+  
